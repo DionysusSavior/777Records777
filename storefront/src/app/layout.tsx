581a706ec121a101fef9e3db4b8ca0eec57d1c38
Dion Savior <dionysussavior@outlook.com>
2026-04-03 15:44:12 -0400

    addin ned tag

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -31,6 +31,13 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="dark" className="dark">
       <head>
+        <Script id="gtm" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TBW33G24');`}
+        </Script>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-11321668434"
           strategy="afterInteractive"
@@ -45,6 +52,14 @@ export default function RootLayout(props: { children: React.ReactNode }) {
         </Script>
       </head>
       <body className="bg-slate-950 text-slate-100">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TBW33G24"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <main className="relative">
           <SplashScreen>{props.children}</SplashScreen>
         </main>
