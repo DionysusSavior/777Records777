a184bea2ba5af0a37180b5244f3ee88a29c93048
Dion Savior <dionysussavior@outlook.com>
2026-04-17 00:16:30 -0400

    g

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
+import Script from "next/script"
 import "styles/globals.css"
 import SplashScreen from "@modules/layout/components/splash-screen"
 import "../styles/animations.css"
@@ -29,7 +30,24 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="dark" className="dark">
+      <head>
+        <Script id="gtm" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WVRWRWCJ');`}
+        </Script>
+      </head>
       <body className="bg-slate-950 text-slate-100">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WVRWRWCJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <main className="relative">
           <SplashScreen>{props.children}</SplashScreen>
         </main>
