625557a9ddeeaefb16b65c3f72ff05efa2304222
Dion Savior <dionysussavior@outlook.com>
2026-04-17 00:13:42 -0400

    deleting google code

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -1,9 +1,7 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
-import Script from "next/script"
 import "styles/globals.css"
 import SplashScreen from "@modules/layout/components/splash-screen"
-import PageViewConversion from "@modules/common/components/page-view-conversion"
 import "../styles/animations.css"
 
 export const metadata: Metadata = {
@@ -31,22 +29,7 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="dark" className="dark">
-      <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-11321668434"
-          strategy="afterInteractive"
-        />
-        <Script id="google-ads-tag" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-11321668434');
-          `}
-        </Script>
-      </head>
       <body className="bg-slate-950 text-slate-100">
-        <PageViewConversion />
         <main className="relative">
           <SplashScreen>{props.children}</SplashScreen>
         </main>
